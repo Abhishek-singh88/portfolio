7a1e0d0c39e2218f58793fd5d955dad52b835375
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-29 20:09:10 +0530

    fixing

---
+++ src/data/hackthons.ts
@@ -0,0 +1,35 @@
+// hacktho.js
+export const hackathons = [
+  {
+    name: "Google GenAI Exchange Hackathon",
+    title: "1st Runner-Up (Student Track)",
+    date: "Sept 2024",
+    tech: ["Nextjs", "Node.js", "Express", "3web.js", "Gemini AI", "clerk", "TypeScript"],
+    icon: "🥈",
+    link: "https://example.com/genai"
+  },
+  {
+    name: "AIA Chain Inaugural Hackathon",
+    title: "Consumer Application Winner 🏆",
+    date: "Nov 2024",
+    tech: ["Nextjs", "solidity", "Prisma", "EVM", "remix", "Node.js", "Express", "Gemini Ai", "Etherjs", "TypeScript"],
+    icon: "🏆",
+    link: "https://x.com/yourproject"
+  },
+  {
+    name: "Telos Mini Hackathon",
+    title: "Finalist",
+    date: "Feb 2025",
+    tech: ["React", "Rust", "ICP", "Next.js"],
+    icon: "🎯",
+    link: "https://github.com/yourrepo"
+  },
+  {
+    name: "Filecoin Repos Challenge",
+    title: "Top 10 Teams",
+    date: "Jan 2025",
+    tech: ["Filecoin", "Lighthouse", "TypeScript"],
+    icon: "🔥",
+    link: "https://filecoin.io"
+  }
+];
---
+++ src/data/projects.ts
@@ -0,0 +1,37 @@
+
+export const projects = [
+
+  {
+      title: "CORO TASHI",
+    description: "A decentralized DeFi platform...",
+    tech: ["Tailwind CSS", "Solidity", "Prisma", "Remix"],
+    github: "#",
+    demo: "#",
+    image: ""
+  },
+  {
+    title: "Decentralized Drive",
+    description: "Built a decentralized application (dApp) that enables users to store and share images securely using IPFS (InterPlanetary File System) and Ethereum blockchain. Users can connect their crypto wallets, upload images to the IPFS network, and share files with other Ethereum addresses. This project emphasizes data ownership, privacy, and decentralization, eliminating reliance on centralized servers.",
+    tech: ["React", "solidity", "web3.js", "IPFS","pinata", "Alchemy"],
+    github: "https://github.com/Abhishek-singh88/Decentralize-drive",
+    demo: "https://decentralize-drive77.netlify.app/",
+    image: "/Decentralized.png"
+  },
+  {
+    title: "EDU LEGACY",
+    description: "Secure your academic legacy...",
+    tech: ["TypeScript", "Solana", "Rust"],
+    github: "#",
+    demo: "#",
+    image: ""
+  },
+  {
+    title: "SKILL LINK",
+    description: "A DApp for verified skills...",
+    tech: ["Next.js", "Solidity", "Ethers.js"],
+    github: "#",
+    demo: "#",
+    image: ""
+  },
+  // add more if needed
+];
