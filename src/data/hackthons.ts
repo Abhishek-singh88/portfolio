91b37c0f97be33e98ea44c14209fdf6582f28be2
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-30 11:50:41 +0530

    fixing

--- src/data/hackthons.ts
+++ src/data/hackthons.ts
@@ -1,35 +1,12 @@
 // hacktho.js
 export const hackathons = [
   {
-    name: "Google GenAI Exchange Hackathon",
-    title: "1st Runner-Up (Student Track)",
-    date: "Sept 2024",
-    tech: ["Nextjs", "Node.js", "Express", "3web.js", "Gemini AI", "clerk", "TypeScript"],
-    icon: "🥈",
-    link: "https://example.com/genai"
+    name: "Integrate Civic Auth into Your Application",
+    title: "Participant – Built Smart PagePeer",
+    date: "june 2025",
+    tech: ["Nextjs", "Node.js", "MongoDb","TypeScript"],
+    icon: "👨‍💻",
+    link: "https://page-peer.vercel.app/",
+    site:"https://dorahacks.io/hackathon/civic-auth/buidl"
   },
-  {
-    name: "AIA Chain Inaugural Hackathon",
-    title: "Consumer Application Winner 🏆",
-    date: "Nov 2024",
-    tech: ["Nextjs", "solidity", "Prisma", "EVM", "remix", "Node.js", "Express", "Gemini Ai", "Etherjs", "TypeScript"],
-    icon: "🏆",
-    link: "https://x.com/yourproject"
-  },
-  {
-    name: "Telos Mini Hackathon",
-    title: "Finalist",
-    date: "Feb 2025",
-    tech: ["React", "Rust", "ICP", "Next.js"],
-    icon: "🎯",
-    link: "https://github.com/yourrepo"
-  },
-  {
-    name: "Filecoin Repos Challenge",
-    title: "Top 10 Teams",
-    date: "Jan 2025",
-    tech: ["Filecoin", "Lighthouse", "TypeScript"],
-    icon: "🔥",
-    link: "https://filecoin.io"
-  }
 ];
