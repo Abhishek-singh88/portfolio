870fe5bad49b5aba489ebcf5583eed700984bf53
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-07-17 23:38:14 +0530

    updating projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,22 @@
 
 export const projects = [
 
+    {
+    title: "NostrExtract",
+    description: "NostrExtract delivers real-time article summarization using Gemini AI, seamlessly integrated as a Smart Widget. It empowers users in the decentralized Nostr network to quickly grasp key insights from any article—boosting productivity, enhancing content accessibility, and reducing information overload without leaving their ecosystem.",
+    tech: ["Nextjs", "Tailwind CSS", "shadcn", "Gemini API"],
+    github: "https://github.com/Abhishek-singh88/NostrExtract",
+    demo: "https://nostr-extract.vercel.app/",
+    image: "/NostrExtract.png"
+  },
+   {
+    title: "Decentralized Drive",
+    description: "Built a decentralized application (dApp) that enables users to store and share images securely using IPFS (InterPlanetary File System) and Ethereum blockchain. Users can connect their crypto wallets, upload images to the IPFS network, and share files with other Ethereum addresses. This project emphasizes data ownership, privacy, and decentralization, eliminating reliance on centralized servers.",
+    tech: ["React", "solidity", "web3.js", "IPFS","pinata", "Alchemy"],
+    github: "https://github.com/Abhishek-singh88/Decentralize-drive",
+    demo: "https://decentralize-drive77.netlify.app/",
+    image: "/Decentralized.png"
+  },
   {
       title: "PagePeer",
     description: "PagePeer is a decentralized e-Library platform revolutionizing how we access, own, and experience digital books. Powered by Web3, it empowers users to explore an expansive collection, mint books as NFTs, and build a secure, censorship-resistant personal library.",
@@ -8,14 +24,6 @@ export const projects = [
     github: "https://github.com/Abhishek-singh88/PagePeer",
     demo: "https://page-peer.vercel.app/",
     image: "/Pagepeer.png"
-  },
-  {
-    title: "Decentralized Drive",
-    description: "Built a decentralized application (dApp) that enables users to store and share images securely using IPFS (InterPlanetary File System) and Ethereum blockchain. Users can connect their crypto wallets, upload images to the IPFS network, and share files with other Ethereum addresses. This project emphasizes data ownership, privacy, and decentralization, eliminating reliance on centralized servers.",
-    tech: ["React", "solidity", "web3.js", "IPFS","pinata", "Alchemy"],
-    github: "https://github.com/Abhishek-singh88/Decentralize-drive",
-    demo: "https://decentralize-drive77.netlify.app/",
-    image: "/Decentralized.png"
   },
   {
     title: "Smart Tailor",
