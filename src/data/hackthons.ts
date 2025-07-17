f4847664fe0f87133121f2669aea6bbf27f318ff
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-07-17 23:37:57 +0530

    updating hackthons

--- src/data/hackthons.ts
+++ src/data/hackthons.ts
@@ -1,5 +1,14 @@
 // hacktho.js
 export const hackathons = [
+   {
+    name: "Agentic Mini Apps Hack on Nostr Round 1",
+    title: "Ongoing...",
+    date: "july 2025",
+    tech: ["Nextjs", "Tailwind CSS", "shadcn", "Gemini API"],
+    icon: "👨‍💻",
+    link: "https://nostr-extract.vercel.app/",
+    site:"https://dorahacks.io/hackathon/nostr2100/buidl"
+  },
   {
     name: "Integrate Civic Auth into Your Application",
     title: "Participant – Built Smart PagePeer",
