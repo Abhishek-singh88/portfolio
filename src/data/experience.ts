1a807f8d9c1451a7d6e945da3812cd0256997794
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-10-15 21:43:25 +0530

    adding experience section

---
+++ src/data/experience.ts
@@ -0,0 +1,21 @@
+export interface Experience {
+  company: string;
+  role: string;
+  logo: string;
+  duration: string;
+}
+
+export const experiences: Experience[] = [
+  {
+    company: "Solia Network",
+    role: "Smart Contract Developer",
+    logo: "/solia.jpg",
+    duration: "Oct 2025 – Present",
+  },
+  {
+    company: "Turbin3",
+    role: "Pre Builder",
+    logo: "/turbin3.jpg",
+    duration: "Oct 2025 – Present",
+  },
+];
