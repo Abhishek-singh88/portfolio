95150b3af6a76d3a2c48bd400192a42236122893
Abhishek Singh <abhisheksingh4928@gmail.com>
2026-07-05 21:47:59 +0530

    fix: update experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,11 +6,17 @@ export interface Experience {
 }
 
 export const experiences: Experience[] = [
-    {
+  {
+    company: "Cardium games",
+    role: "Backend developer Intern",
+    logo: "/cardium.jpg",
+    duration: "May 2026 – Present",
+  },  
+  {
     company: "Turbin3",
     role: "Builder",
     logo: "/turbin3.jpg",
-    duration: "Jan 2026 – Present",
+    duration: "Jan 2026 – Mar 2026",
   },
   {
     company: "Solia Network",
